666688c96836361dd63ffa7374642be19a91b169
kutttybay <kutttybay@gmail.com>
2023-10-24 16:02:13 +0600

    Solo5

--- index.js
+++ index.js
@@ -1,118 +1,97 @@
+// Создаем массив для хранения задач
+let tasks = [];
 
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    const name = document.getElementById("name").value;
+// Функция для добавления новой задачи
+function addTask() {
+    const taskText = document.getElementById("new-task").value;
+    if (taskText.trim() !== "") {
+        tasks.push({ text: taskText, completed: false });
+        updateTaskList();
+        document.getElementById("new-task").value = "";
+    }
+}
+
+// Функция для обновления списка задач
+function updateTaskList() {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<input type="checkbox" onchange="toggleTask(${index})" ${task.completed ? "checked" : ""}> ${task.text} <button onclick="deleteTask(${index})">Удалить</button>`;
+        taskList.appendChild(listItem);
+    });
+}
+
+// Функция для удаления задачи
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    updateTaskList();
+}
+
+// Функция для отметки задачи как выполненной
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskList();
+}
+
+
+
+
+// Форма
+function validateForm() {
+    const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
-    
-    let hasError = false;
+    const password = document.getElementById("password").value;
 
-    if (name === "") {
-        hasError = true;
-        document.getElementById("nameError").innerText = "Name is required";
-    } else {
-        document.getElementById("nameError").innerText = "";
+    if (username.trim() === "" || email.trim() === "" || password.trim() === "") {
+        alert("Пожалуйста, заполните все обязательные поля.");
+        return false;
     }
+    return true;
+}
 
-    if (email === "") {
-        hasError = true;
-        document.getElementById("emailError").innerText = "Email is required";
-    } else {
-        document.getElementById("emailError").innerText = "";
+
+
+// Таймер
+
+let timerInterval;
+let countdown = document.getElementById("countdown");
+
+function startTimer() {
+    const duration = parseInt(document.getElementById("duration").value);
+    if (!isNaN(duration) && duration > 0) {
+        let remainingTime = duration;
+        countdown.style.display = "block";
+
+        timerInterval = setInterval(function () {
+            countdown.innerText = remainingTime + " сек.";
+            remainingTime -= 1;
+
+            if (remainingTime < 0) {
+                clearInterval(timerInterval);
+                countdown.style.display = "none";
+                alert("Таймер завершился!");
+            }
+        }, 1000);
     }
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        hasError = true;
-        document.getElementById("emailError").innerText = "Invalid email address";
-    } else {
-        document.getElementById("emailError").innerText = "";
-    }
-
-    if (hasError) {
-        event.preventDefault(); // Prevent form submission if there are errors
-    }
-});
-
-document.getElementById("startCountdown").addEventListener("click", function () {
-    const countdownElement = document.getElementById("countdown");
-    let countdownValue = parseInt(countdownElement.textContent);
-    const interval = setInterval(function () {
-        countdownValue--;
-        countdownElement.textContent = countdownValue;
-        if (countdownValue <= 0) {
-            clearInterval(interval);
-        }
-    }, 1000);
-});
+}
 
 
-// Initialize the popover
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
+
+// Интерактив обьект 
+
+let textHidden = true;
+const hiddenText = document.getElementById("hidden-text");
+
+function toggleText() {
+    textHidden = !textHidden;
+    hiddenText.style.display = textHidden ? "none" : "block";
+}
 
 
-document.getElementById("showHideButton").addEventListener("click", function() {
-    const hiddenContent = document.getElementById("hiddenContent");
-    if (hiddenContent.style.display === "none") {
-        hiddenContent.style.display = "block";
-    } else {
-        hiddenContent.style.display = "none";
-    }
-});
+// Алерт
 
-document.getElementById("toggleAccordion").addEventListener("click", function() {
-    const accordionContent = document.getElementById("accordionContent");
-    if (accordionContent.style.display === "none") {
-        accordionContent.style.display = "block";
-    } else {
-        accordionContent.style.display = "none";
-    }
-});
-
-
-document.getElementById("showAlert").addEventListener("click", function() {
-    alert("Это оповещение о чем-то важном.");
-});
-
-document.getElementById("showPrompt").addEventListener("click", function() {
-    const userInput = prompt("Пожалуйста, введите ваш комментарий:");
-    if (userInput !== null) {
-        alert("Вы ввели следующий комментарий: " + userInput);
-    } else {
-        alert("Вы ничего не ввели.");
-    }
-});
-
-document.getElementById("showConfirm").addEventListener("click", function() {
-    const userConfirmed = confirm("Вы уверены, что хотите удалить этот файл?");
-    if (userConfirmed) {
-        alert("Файл удален.");
-    } else {
-        alert("Удаление отменено.");
-    }
-});
-
-
-// Initialize the popover
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl) 
-})
-
-
-document.getElementById("addTask").addEventListener("click", function () {
-  const taskInput = document.getElementById("task");
-  const taskText = taskInput.value.trim();
-  if (taskText !== "") {
-      const taskList = document.getElementById("taskList");
-      const listItem = document.createElement("li");
-      listItem.textContent = taskText;
-      taskList.appendChild(listItem);
-      taskInput.value = "";
-  }
-});
+function showAlert() {
+    alert("Маладец, ты сделал все !!!");
+}
